c1292f5769e247d6bfbdea08d2da5d98298d0fd3
Kris Norful <krisnorful@gmail.com>
2021-04-15 11:05:41 -0500

    add style to navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,6 +4,7 @@ import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
+import { SearchShow } from "../SearchShow/SearchShow";
 import SearchIcon from "@material-ui/icons/Search";
 
 const useStyles = makeStyles((theme) => ({
@@ -15,16 +16,31 @@ const useStyles = makeStyles((theme) => ({
   },
   toolbar: {
     alignItems: "center",
+    color: "#479AD1",
   },
   title: {
+    display: "flex",
+    alignItems: "center",
     flexGrow: 1,
     alignSelf: "center",
     marginLeft: theme.spacing(25),
+    color: "white",
+    fontFamily: "Dela Gothic One, cursive",
+    fontStyle: "italic",
+  },
+  titleDash: {
+    marginLeft: theme.spacing(1),
+    background:
+      "linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(208,237,241,1) 0%, rgba(71,154,209,1) 50%)",
+    height: "20px",
+    width: "80px",
+    borderRadius: "10px",
   },
   search: {
     marginRight: theme.spacing(25),
   },
   searchIcon: {
+    color: "#D0EDF1",
     fontSize: "2rem",
   },
 }));
@@ -41,8 +57,9 @@ export const Navbar = () => {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar className={classes.toolbar}>
-          <Typography className={classes.title} variant="h5" noWrap>
-            Faceplant
+          <Typography className={classes.title} variant="h4" noWrap>
+            FP
+            <div className={classes.titleDash}></div>
           </Typography>
           <IconButton
             className={classes.search}
@@ -51,11 +68,12 @@ export const Navbar = () => {
           >
             <SearchIcon
               onClick={handleSearchClick}
-              style={{ fontSize: "2rem" }}
+              className={classes.searchIcon}
             />
           </IconButton>
         </Toolbar>
       </AppBar>
+      {searchClick ? <SearchShow /> : null}
     </div>
   );
 };
